bb070f5235a2c0bbcfdbb8335666d81cd887d9a4
sachin-source <sachinmgvt@gmail.com>
2024-11-28 16:16:41 +0530

    importing database method via manager

---
+++ src/main/java/com/loose/coupling/UserDatabase.java
@@ -0,0 +1,8 @@
+package com.tight.coupling;
+
+public class UserDatabase {
+    public String getUserDetails(){
+        // Accessing the database directly
+        return "User details from Database";
+    }
+}
