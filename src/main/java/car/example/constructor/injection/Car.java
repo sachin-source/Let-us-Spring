bb17373f8fd8e0070fb913e19462d897bb226f16
sachin-source <sachinmgvt@gmail.com>
2024-12-07 15:34:59 +0530

    displaying car specification details

--- src/main/java/car/example/constructor/injection/Car.java
+++ src/main/java/car/example/constructor/injection/Car.java
@@ -1,4 +1,9 @@
 package car.example.constructor.injection;
 
 public class Car {
+    private Specification specification;
+
+    public void displayDetails(){
+        System.out.println( "Car details : " + specification.toString());
+    }
 }
