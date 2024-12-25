60b24b59d875429d2b28645e5860bd78cc6fa614
sachin-source <sachinmgvt@gmail.com>
2024-12-25 16:58:17 +0530

    transit from XML based approach to annotation based approach

--- src/main/java/com/example/componentscan/annotation/App.java
+++ src/main/java/com/example/componentscan/annotation/App.java
@@ -1,11 +1,11 @@
 package com.example.componentscan.annotation;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("componentScanDemo.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Employee employee = context.getBean("employee", Employee.class);
         System.out.println(employee.toString());
     }
