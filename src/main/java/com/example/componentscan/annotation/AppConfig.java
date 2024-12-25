6a0758ef597e843f9b28dacf9c2abca91c0c4099
sachin-source <sachinmgvt@gmail.com>
2024-12-25 16:46:29 +0530

    AppConfig Setup for annotation based application

--- src/main/java/com/example/componentscan/annotation/AppConfig.java
+++ src/main/java/com/example/componentscan/annotation/AppConfig.java
@@ -1,4 +1,9 @@
 package com.example.componentscan.annotation;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // Instructing the spring to refer below defined package for all the materials
+@ComponentScan(basePackages = "com.example.componentscan.annotation") // This is to define the package
 public class AppConfig {
 }
