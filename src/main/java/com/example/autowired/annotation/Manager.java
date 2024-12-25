96e5f41e8894a4a4a36599d91f57bd590dcd0436
sachin-source <sachinmgvt@gmail.com>
2024-12-25 18:33:38 +0530

    Qualifier annotation

--- src/main/java/com/example/autowired/annotation/Manager.java
+++ src/main/java/com/example/autowired/annotation/Manager.java
@@ -1,11 +1,13 @@
 package com.example.autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Manager {
     @Autowired // field injection
+    @Qualifier("employee") // This annotation is used when we have multiple beans registered with the same type
     private Employee employee;
 
 //    @Autowired // constructor injection
